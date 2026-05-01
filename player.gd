2315c7a89e130778fe8505922d7b2648596a07cf
lucaslav05 <lucaslaviolette05@gmail.com>
2026-04-30 19:44:23 -0700

    added inventory and begun working on item pickup, also added crop scene and script

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var inv: Inv
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -8,3 +9,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
+
+func collect(item):
+	inv.insert(item)

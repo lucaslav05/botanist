c8114feafd0e1f399b4186ce60517634dffce967
lucaslav05 <lucaslaviolette05@gmail.com>
2026-04-30 17:22:55 -0700

    Added Starting Scene, Basic Movement, And Tile Collisions

---
+++ tile_map_layer.gd
@@ -0,0 +1,11 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var filled_tiles := get_used_cells()
+	for filled_tile: Vector2i in filled_tiles:
+		var neighbouring_tiles := get_surrounding_cells(filled_tile)
+		for neighbour: Vector2i in neighbouring_tiles:
+			if get_cell_source_id(neighbour) == -1:
+				set_cell(neighbour, 0, Vector2i.ZERO)
